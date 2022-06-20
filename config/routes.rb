e285d05b505e60ec7cb3b42e3bfa5a60f5a4f5c0
cynthiainga <candyinga67@gmail.com>
2022-06-20 16:26:46 +0200

    Add the basic route configuration and for devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'foods#index'
+  resources :foods, only: [:index]
+  resources :recipes, only: [:index, :show]
+  resources :public_recipes, only: [:index]
+  resources :inventories_list, only: [:index]
 end
