3179368e35ee8af69157a2583ed33583c4cfe0d4
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-21 12:38:00 +0100

    Add associations.

--- db/schema.rb
+++ db/schema.rb
@@ -38,6 +38,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_21_111927) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
@@ -49,7 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_06_21_111927) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
