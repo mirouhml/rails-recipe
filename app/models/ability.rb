9f62e2208dd7c3572361ce0090bad1cdb657a206
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-21 23:58:27 +0100

    Add ability file for cancancan.

---
+++ app/models/ability.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    cannot :read, :all
+    return unless user.present?
+
+    can :read, :all, user_id: user.id
+    can :create, Inventory, user_id: user.id
+    can :destroy, Inventory, user_id: user.id
+  end
+end
