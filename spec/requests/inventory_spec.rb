8660f7857339b585cd468892b8b28699ca0706a1
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-22 00:03:02 +0100

    Generate views and controllers.

---
+++ spec/requests/inventory_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe "Inventories", type: :request do
+  describe "GET /index" do
+    it "returns http success" do
+      get "/inventory/index"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /show" do
+    it "returns http success" do
+      get "/inventory/show"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /new" do
+    it "returns http success" do
+      get "/inventory/new"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /create" do
+    it "returns http success" do
+      get "/inventory/create"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET /destroy" do
+    it "returns http success" do
+      get "/inventory/destroy"
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+end
