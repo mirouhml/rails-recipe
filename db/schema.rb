e50fbdba853ed728196068249954e28d6fc048d3
Dagi <dagi.zewdu.dz@gmail.com>
2022-06-22 10:35:29 +0300

    Change time colums to recipe

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_01_31_173225) do
+ActiveRecord::Schema[7.0].define(version: 2022_02_02_201017) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -36,13 +36,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_01_31_173225) do
 
   create_table "recipes", force: :cascade do |t|
     t.string "name"
-    t.time "preparation_time"
-    t.time "cooking_time"
     t.text "description"
     t.boolean "public"
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float "preparation_time"
+    t.float "cooking_time"
     t.index ["user_id"], name: "index_recipes_on_user_id"
   end
 
@@ -50,6 +50,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_01_31_173225) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "foods", "users"
