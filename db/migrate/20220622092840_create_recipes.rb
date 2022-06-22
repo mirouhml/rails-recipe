2180eb56d3596cb6e9b940ac611a2f1907164f42
cynthiainga <candyinga67@gmail.com>
2022-06-22 13:35:07 +0200

    Generate migration of foods,recipes and recipes_foods

--- db/migrate/20220622092840_create_recipes.rb
+++ db/migrate/20220622092840_create_recipes.rb
@@ -1,11 +1,9 @@
-class CreateFoods < ActiveRecord::Migration[7.0]
+class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
-    create_table :foods do |t|
+    create_table :recipes do |t|
       t.string :name
-      t.string :measurement_unit
-      t.decimal :price
       t.references :user, foreign_key: true, null: false, column_name: :user_id
-      
+
       t.timestamps
     end
   end
