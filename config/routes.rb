630c29129b83bd9f993fc2ea59e91a538bedee10
Dagi <dagi.zewdu.dz@gmail.com>
2022-06-22 15:19:07 +0300

    Added some styling

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,14 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root 'food#index'
+  devise_scope :user do
+    get '/user/sign_out' => 'devise/sessions#destroy'
+  end
   resources :food, only: [:index, :new, :create, :destroy]
-  resources :recipes, only: [:index, :show,:new,:create, :destroy]
+  resources :recipes do
+    resources :recipe_foods
+  end
   resources :public_recipes, only: [:index]
   resources :inventories_list, only: [:index]
+  delete '/recipes/:recipe_id/recipe_foods/:id', to: 'recipe_foods#destroy', as: "delete_recipe_food"
 end
