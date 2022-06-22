4d936192423c0dd3dafe5a4f4615e19321f10664
cynthiainga <candyinga67@gmail.com>
2022-06-22 13:32:59 +0200

    Controller for inventory_food

--- app/controllers/inventory_foods_controller.rb
+++ app/controllers/inventory_foods_controller.rb
@@ -1,7 +1,23 @@
 class InventoryFoodsController < ApplicationController
-  def new; end
+  def new
+    @inventory_food = InventoryFood.new
+    @foods = current_user.foods
+  end
 
-  def create; end
+  def create
+    @foods = current_user.foods
+    inventory_food = InventoryFood.new(inventory_food_params)
+    inventory_food.inventory_id = params[:inventory_id]
+
+    if inventory_food.save
+      redirect_to user_inventory_path(current_user.id, params[:inventory_id]),
+                  notice: 'Inventory food added succesfully'
+    else
+      redirect_to new_user_inventory_inventory_food_path(current_user.id, params[:inventory_id]),
+                  alert: 'Duplicate entry. Please choose another food'
+
+    end
+  end
 
   def destroy
     inventory_details = InventoryFood.find_by(id: params[:id])
