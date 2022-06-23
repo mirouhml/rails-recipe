0ee5a7d38e3f7fa77870465ec4b434cdd6a3c6e3
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-23 09:18:08 +0100

    Show delete buttons only for users who created the items.

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,14 +5,8 @@ class Ability
     cannot :read, :all
     return unless user.present?
 
-    can :read, :all, user_id: user.id
-    can :create, Inventory, user_id: user.id
-    can :destroy, Inventory, user_id: user.id
-    can :create, Food, user_id: user.id
-    can :destroy, Food, user_id: user.id
-    can :create, RecipeFood, user_id: user.id
-    can :destroy, RecipeFood, user_id: user.id
-    can :create, Recipe, user_id: user.id
-    can :destroy, Recipe, user_id: user.id
+    can :read, Food
+    can :create, :all
+    can :destroy, :all, user_id: user.id
   end
 end
