98bd0cc50606b4d80e924cb47884ea505f1b2ef0
Ammar Hamlaoui <amarhamlaoui@gmail.com>
2022-06-24 13:41:42 +0100

    Fix N+1 query issues and implement required changes.

--- app/controllers/public_recipes_controller.rb
+++ app/controllers/public_recipes_controller.rb
@@ -1,6 +1,6 @@
 class PublicRecipesController < ApplicationController
   def index
-    @recipes = Recipe.includes(:recipe_foods).where(public: true).order(id: :desc)
+    @recipes = Recipe.includes(:recipe_foods, :user).where(public: true).order(id: :desc)
     @recipe_foods = RecipeFood.includes(:food).where(recipe_id: @recipes.ids)
   end
 end
