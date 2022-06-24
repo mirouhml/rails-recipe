0ff95fe490d34a27b08a77042b667acc04e8657a
Dagi <dagi.zewdu.dz@gmail.com>
2022-06-24 13:50:52 +0300

    fixed lintor errors

--- spec/features/recipes_spec.rb
+++ spec/features/recipes_spec.rb
@@ -1,16 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Food, type: :model do
-before(:each) do
+  before(:each) do
     @user = User.new(name: 'Test User', email: 'exemple@gmail.com', password: 'password')
     @recipe = Recipe.new(name: 'Test Recipe', user: @user)
     @recipe_food = RecipeFood.new(recipe: @recipe, food: Food.new(name: 'Test Food', user: @user), quantity: 10)
-
   end
 
-    it 'recipe belongs to a user' do
+  it 'recipe belongs to a user' do
     expect(@recipe.user_id).to eq(@user.id)
-    end
+  end
 
   it 'reciped food has the correct recipe' do
     expect(@recipe_food.recipe).to eq(@recipe)
